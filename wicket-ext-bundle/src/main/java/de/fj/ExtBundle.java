f6afb47404c155de5ddf6a13918a8eb1072c44d4
fjackstadt <frido37@users.noreply.github.com>
2010-02-21 15:47:51 +0000

    .

---
+++ wicket-ext-bundle/src/main/java/de/fj/ExtBundle.java
@@ -0,0 +1,13 @@
+package de.fj;
+
+
+public class ExtBundle {
+	
+	public static final String EXT_BASE_DEBUG = "extjs/adapter/ext/ext-base-debug.js";
+	public static final String EXT_BASE_DEPLOY = "extjs/adapter/ext/ext-base.js";
+	public static final String EXT_ALL_DEBUG = "extjs/ext-all-debug.js";
+	public static final String EXT_ALL_DEPLOY = "extjs/ext-all.js";
+	
+	public static final String EXT_ALL_STYLE = "extjs/resources/css/ext-all.css";
+	
+}
