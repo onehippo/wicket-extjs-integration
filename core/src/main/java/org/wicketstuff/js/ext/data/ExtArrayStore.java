a600167a9aae5921ffb89d8adccc0734b8f13ad4
Frank van Lankvelt <frnk@a-eskwadraat.nl>
2013-04-26 09:31:33 +0000

    array store should invoke observer's onRenderProperties

--- core/src/main/java/org/wicketstuff/js/ext/data/ExtArrayStore.java
+++ core/src/main/java/org/wicketstuff/js/ext/data/ExtArrayStore.java
@@ -35,6 +35,8 @@ public class ExtArrayStore<T> extends ExtStore<T> {
 
     @Override
     public void onRenderProperties(JSONObject properties) throws JSONException {
+        super.onRenderProperties(properties);
+
         JSONArray jsonFields = new JSONArray();
         for (ExtDataField field : getFields()) {
             JSONObject jsonField = new JSONObject();
